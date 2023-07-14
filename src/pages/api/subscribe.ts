b0ecdae0aad9ef3e502a5e706f3a53041113e438
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-07-14 21:41:54 +0300

    Add user type

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -2,6 +2,15 @@ import { stripe } from "@/src/services/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const session = await getSession({ req });
