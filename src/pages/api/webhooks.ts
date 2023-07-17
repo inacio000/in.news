922f67ef7dc718237a89fb866126d14ef0dbf3ae
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-07-18 00:35:15 +0300

    Add news relevant events to listen to

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { stripe } from "@/src/services/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 // Convertendo Readable streaming em um objeto (requisição)
 async function buffer(readable: Readable) {
@@ -24,7 +25,10 @@ export const config = {
 
 // Uma Array sem elementos duplicados
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.created',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -44,14 +48,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event;
 
         if (relevantEvents.has(type)) {
-            // do something
-            console.log('Event received: ', event)
-        }
+                try {
+                    switch (type) {
+                        case 'customer.subscription.updated':
+                        case 'customer.subscription.deleted':
+                            const subscription = event.data.object as Stripe.Subscription;
+
+                            await saveSubscription(
+                                subscription.id,
+                                subscription.customer.toString(),
+                                false,
+                            )
+                            break;
+
+                        case 'checkout.session.completed':
+
+                            const checkoutSession = event.data.object as Stripe.Checkout.Session
+                            
+                            await saveSubscription(
+                                checkoutSession.subscription?.toString()!,
+                                checkoutSession.customer?.toString()!,
+                                true,
+                            )
+                            break;
+                        default:
+                            throw new Error('Unhandled event.')
+                    }
+                } catch (err) {
+                    return res.json({ error: 'Webhook handle failed.' })
+                }
+            }
 
         res.json({ received: true})
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method Not Allowed')
     }
-
 }
